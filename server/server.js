4a6a5f53be91c739302e4350998f68d4913e1fa2
brainbreakers5 <brainbreakers5@gmail.com>
2026-03-14 20:37:38 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -163,11 +163,12 @@ app.use('/api/activity-logs', activityLogRoutes);
 app.use('/api/status', statusRoutes);
 
 // Production Static Files
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../client/dist')));
-    app.use((req, res) => {
-        res.send("server running");
-    });
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../client/dist/index.html"));
+  });
 }
 
 const PORT = process.env.PORT || 5000;
