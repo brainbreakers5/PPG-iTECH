3cc83e259a98b796ea640c7976c62db89f837776
brainbreakers5 <brainbreakers5@gmail.com>
2026-03-17 00:40:41 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -161,7 +161,7 @@ app.use('/api/status', statusRoutes);
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../client/dist")));
 
-  app.get("*", (req, res) => {
+  app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "../client/dist/index.html"));
   });
 }
