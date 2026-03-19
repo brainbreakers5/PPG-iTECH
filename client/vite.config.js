15bddbf2cb5e21d582f80c428232f7b6c6b6d90f
brainbreakers5 <brainbreakers5@gmail.com>
2026-03-19 20:43:44 +0530

    Change PWA registration type and update skipWaiting

--- client/vite.config.js
+++ client/vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
   plugins: [
     react(),
     VitePWA({
-      registerType: 'autoUpdate',
+      registerType: 'prompt',
       manifestFilename: 'manifest.json',
       includeAssets: ['ppg-logo.png', 'ppg-bg.jpg'],
       manifest: {
@@ -49,7 +49,7 @@ export default defineConfig({
         ],
         cleanupOutdatedCaches: true,
         clientsClaim: true,
-        skipWaiting: true, // Automatically update without prompt
+        skipWaiting: false, // Wait for manual reload
       }
     })
   ],
