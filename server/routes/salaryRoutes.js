ad609c98cf130f4c6b51af440b9dd66dbef65f45
brainbreakers5 <brainbreakers5@gmail.com>
2026-03-21 15:33:40 +0530

    Update salaryRoutes.js

--- server/routes/salaryRoutes.js
+++ server/routes/salaryRoutes.js
@@ -1,12 +1,19 @@
 const express = require('express');
 const router = express.Router();
-const { calculateSalary, getSalaryRecords, updateSalaryStatus, publishSalaries } = require('../controllers/salaryController');
+const {
+    calculateSalary,
+    getSalaryRecords,
+    getDailyBreakdown,
+    updateSalaryStatus,
+    publishSalaries
+} = require('../controllers/salaryController');
 const { protect, restrictTo } = require('../middleware/authMiddleware');
 
 router.use(protect);
 
 router.post('/calculate', restrictTo('admin'), calculateSalary);
 router.post('/publish', restrictTo('admin'), publishSalaries);
+router.get('/daily', getDailyBreakdown);
 router.get('/', getSalaryRecords);
 router.put('/:id/status', restrictTo('admin'), updateSalaryStatus);
 
